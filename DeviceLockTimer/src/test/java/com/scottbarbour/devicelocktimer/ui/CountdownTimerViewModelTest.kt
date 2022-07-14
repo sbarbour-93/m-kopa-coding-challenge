a99078273aca76016437f12f3dd1a7f3a6634964
Scott Barbour <sbarbour1993@gmail.com>
2022-07-14 18:13:48 +0200

    - unit tests added for the view model layer

---
+++ DeviceLockTimer/src/test/java/com/scottbarbour/devicelocktimer/ui/CountdownTimerViewModelTest.kt
@@ -0,0 +1,51 @@
+package com.scottbarbour.devicelocktimer.ui
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.scottbarbour.devicelocktimer.data.CountdownTimerRepository
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import kotlin.test.assertEquals
+
+
+@ExperimentalCoroutinesApi
+class CountdownTimerViewModelTest {
+
+    @get:Rule
+    val rule: TestRule = InstantTaskExecutorRule()
+
+    @MockK
+    private lateinit var repository: CountdownTimerRepository
+
+    private lateinit var viewModel: CountdownTimerViewModel
+
+    private val expectedTimerValue = "05:00:00"
+
+    @Before
+    fun setup() {
+        Dispatchers.setMain(StandardTestDispatcher())
+
+        MockKAnnotations.init(this, relaxed = true)
+        coEvery { repository.getTimeUntilDeviceLocks() } returns expectedTimerValue
+
+        viewModel = CountdownTimerViewModel(repository)
+    }
+
+    @Test
+    fun `start timer method calls data layer and posts result to live data property`() = runTest {
+        viewModel.startTimer()
+        advanceUntilIdle()
+        assertEquals(expectedTimerValue, viewModel.timeRemainingUntilDeviceLocks.value)
+    }
+
+}
